abf47e314fbb5dd74ce8e4da2aa8a6af0612fb6d
veeteixeira <veronica.teixeira@sptech.school>
2022-05-30 15:33:26 -0300

    site atualizado

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -25,7 +25,7 @@ function cadastrar(nome, razao, cnpj, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', ${razao}, ${cnpj}, '${email}', '${senha}');
+        INSERT INTO usuario (nome, razao, cnpj, email, senha) VALUES ('${nome}', ${razao}, ${cnpj}, '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
